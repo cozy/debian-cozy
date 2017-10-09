aa3a8ddddea22fecc9ee2ed16d6a375f44039bdb
aeris <aeris@cozycloud.cc>
2017-10-09 16:36:27 +0200

    Create stretch chroot, not jessie

--- konnector-create-chroot.sh
+++ konnector-create-chroot.sh
@@ -8,7 +8,7 @@ debootstrap --variant=minbase --include="${DEBIAN_INCLUDE_PACKAGES}" \
 	stretch "${CHROOT_DIRECTORY}"
 
 chroot "${CHROOT_DIRECTORY}" /bin/bash <<EOF
-	echo "deb https://deb.nodesource.com/node_8.x jessie main" > "/etc/apt/sources.list.d/nodesource.list"
+	echo "deb https://deb.nodesource.com/node_8.x stretch main" > "/etc/apt/sources.list.d/nodesource.list"
 	curl -s "https://deb.nodesource.com/gpgkey/nodesource.gpg.key" | apt-key --keyring "/etc/apt/trusted.gpg.d/nodesource.gpg" add -
 	apt update
 	apt install -y nodejs
