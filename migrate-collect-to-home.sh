1088e02e54bd2094bd42112329d406e8f73b4bd8
Nicolas Frandeboeuf <mail@nicofrand.eu>
2018-11-21 14:39:42 +0100

    Check if apps exist before installing/uninstalling them

--- migrate-collect-to-home.sh
+++ migrate-collect-to-home.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 echo "Migrating collect to home"
 cozy-stack instances ls | awk '{print $1}' | while read domain; do
-	echo "  Migrating ${domain}"
-	echo "    Installing home"
-	cozy-stack apps install home --domain "${domain}" 1>/dev/null
-	echo "    Uninstalling collect"
-	cozy-stack apps uninstall collect --domain "${domain}" 1>/dev/null
+        echo "  Migrating ${domain}"
+        apps_list=$(cozy-stack apps ls --domain "${domain}" | grep "home")
+        app_installed=$(echo "${apps_list}" | grep "home")
+        if [ -n "${app_installed}" ]; then
+                echo "    home already installed, nothing to do"
+        else
+                echo "    Installing home"
+                cozy-stack apps install home --domain "${domain}" 1>/dev/null
+        fi
+
+        app_installed=$(echo "${apps_list}" | grep "collect")
+        if [ -n "${app_installed}" ]; then
+                echo "    Uninstalling collect"
+                cozy-stack apps uninstall collect --domain "${domain}" 1>/dev/null
+        else
+                echo "    collect not installed, nothing to do"
+        fi
 done
