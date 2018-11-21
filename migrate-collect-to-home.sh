88a2f873f06080a6b19aac1a815a75c5d44bdfea
Nicolas Frandeboeuf <mail@nicofrand.eu>
2018-11-21 14:59:36 +0100

    Fix grep

--- migrate-collect-to-home.sh
+++ migrate-collect-to-home.sh
@@ -2,7 +2,7 @@
 echo "Migrating collect to home"
 cozy-stack instances ls | awk '{print $1}' | while read domain; do
         echo "  Migrating ${domain}"
-        apps_list=$(cozy-stack apps ls --domain "${domain}" | grep "home")
+        apps_list=$(cozy-stack apps ls --domain "${domain}")
         app_installed=$(echo "${apps_list}" | grep "home")
         if [ -n "${app_installed}" ]; then
                 echo "    home already installed, nothing to do"
