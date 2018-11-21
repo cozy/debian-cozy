98a1b01f92fe8d86c8ab50728f9e41977c61763e
Nicolas Frandeboeuf <mail@nicofrand.eu>
2018-11-21 15:17:23 +0100

    Use cozy-stack show returned status instead of grepping on lsit

--- migrate-collect-to-home.sh
+++ migrate-collect-to-home.sh
@@ -2,18 +2,19 @@
 echo "Migrating collect to home"
 cozy-stack instances ls | awk '{print $1}' | while read domain; do
         echo "  Migrating ${domain}"
-        apps_list=$(cozy-stack apps ls --domain "${domain}")
-        app_installed=$(echo "${apps_list}" | grep "home")
-        if [ -n "${app_installed}" ]; then
+        cozy-stack apps show home --domain "${domain}" &>/dev/null
+        if [ $? ]; then
                 echo "    home already installed, nothing to do"
         else
                 echo "    Installing home"
+                exit 0
                 cozy-stack apps install home --domain "${domain}" 1>/dev/null
         fi
 
-        app_installed=$(echo "${apps_list}" | grep "collect")
-        if [ -n "${app_installed}" ]; then
+        cozy-stack apps show collect --domain "${domain}" &>/dev/null
+        if [ ! $? ]; then
                 echo "    Uninstalling collect"
+                exit 0
                 cozy-stack apps uninstall collect --domain "${domain}" 1>/dev/null
         else
                 echo "    collect not installed, nothing to do"
