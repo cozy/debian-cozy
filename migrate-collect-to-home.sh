f8bf46ea04e00f5360fe1b9269e9af77df948f29
Nicolas Frandeboeuf <mail@nicofrand.eu>
2018-11-21 15:18:10 +0100

    Remove exit -_-

--- migrate-collect-to-home.sh
+++ migrate-collect-to-home.sh
@@ -6,15 +6,13 @@ cozy-stack instances ls | awk '{print $1}' | while read domain; do
         if [ $? ]; then
                 echo "    home already installed, nothing to do"
         else
-                echo "    Installing home"
-                exit 0
+                echo "    Installing home"                
                 cozy-stack apps install home --domain "${domain}" 1>/dev/null
         fi
 
         cozy-stack apps show collect --domain "${domain}" &>/dev/null
         if [ ! $? ]; then
-                echo "    Uninstalling collect"
-                exit 0
+                echo "    Uninstalling collect"                
                 cozy-stack apps uninstall collect --domain "${domain}" 1>/dev/null
         else
                 echo "    collect not installed, nothing to do"
