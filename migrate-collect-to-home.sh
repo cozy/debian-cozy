815a6a6825e00a5868f50e4d6daf47555f8a8018
Nicolas Frandeboeuf <mail@nicofrand.eu>
2018-12-05 15:50:14 +0000

    Check if apps exist before installing/uninstalling them

--- migrate-collect-to-home.sh
+++ migrate-collect-to-home.sh
@@ -1,11 +1,43 @@
 #!/usr/bin/env bash
 export COZY_ADMIN_PASSWORD="$(cat /etc/cozy/.cozy-admin-passphrase)"
+COCLYCO="$(which cozy-coclyco)"
+
+function coclyco {
+	[ -n "${COCLYCO}" ] && "${COCLYCO}" "${@}"
+}
+
+function app_installed {
+	DOMAIN="${1}"
+	APP="${2}"
+	cozy-stack apps show --domain "${DOMAIN}" "${APP}" &>/dev/null
+}
+
+function install_app {
+	DOMAIN="${1}"
+	APP="${2}"
+	if app_installed "${@}"; then
+		echo "    App ${APP} already installed, nothing to do"
+	else
+		echo "    Installing app ${APP}"
+		cozy-stack apps install --domain "${DOMAIN}" "${APP}" 1>/dev/null
+		coclyco regenerate "${DOMAIN}"
+	fi
+}
+
+function uninstall_app {
+	DOMAIN="${1}"
+	APP="${2}"
+	if app_installed "${@}"; then
+		echo "    Uninstalling app ${APP}"
+		cozy-stack apps uninstall --domain "${DOMAIN}" "${APP}" 1>/dev/null
+	else
+		echo "    App ${APP} already uninstalled, nothing to do"
+	fi
+}
 
 echo "Migrating collect to home"
 cozy-stack instances ls | awk '{print $1}' | while read domain; do
-	echo "  Migrating ${domain}"
-	echo "    Installing home"
-	cozy-stack apps install home --domain "${domain}" 1>/dev/null
-	echo "    Uninstalling collect"
-	cozy-stack apps uninstall collect --domain "${domain}" 1>/dev/null
+		echo "  Migrating ${domain}"
+		install_app "${domain}" home
+		uninstall_app "${domain}" collect
 done
